ff8355b13d0fcc757946dd307813b9f341db91e9
rprinne <rp.rinne@hotmail.com>
2026-01-02 23:22:44 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+    'jest/globals': true,
+    'node': true,
+    'cypress/globals': true,
+    'commonjs': true,
+    'es2021': true,
+    'jest': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'windows'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 0,
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 'off'
+  },
+  'settings': {
+    'react': {
+      'version': 'detect'
+    }
+  }
+}
